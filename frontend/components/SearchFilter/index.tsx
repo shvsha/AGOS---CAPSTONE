d9ed3f7d51883b83e8527ed1f7963d4acecf65e8
Mika <lilaclilac331@gmail.com>
2026-06-20 21:12:30 +0800

    audit logs

--- frontend/components/SearchFilter/index.tsx
+++ frontend/components/SearchFilter/index.tsx
@@ -16,13 +16,13 @@ interface SearchFilterProps {
 
 export function SearchFilter({ value, onChange, placeholder = "Search...", width = "w-70", height = 'h-9' }: SearchFilterProps) {
   return (
-    <div className={`flex items-center bg-[#FAFCFD] border-2 border-[#C6C6C8] rounded-lg px-3 gap-2 ${height}`}>
+    <div className={`flex items-center bg-[#FAFCFD] border-2 border-[#C6C6C8] rounded-lg px-3 gap-2 ${width} ${height}`}>
       <FaSearch size={18} className="text-[#C6C6C8]" />
       <Input
         value={value}
         onChange={e => onChange(e.target.value)}
         placeholder={placeholder}
-        className={`bg-transparent border-0 rounded-lg placeholder:text-gray text-[#122A48] focus-visible:ring-0 h-full ${width}`}
+        className="bg-transparent border-0 rounded-lg placeholder:text-gray text-[#122A48] text-[12px] focus-visible:ring-0 h-full w-full"
       />
     </div>
   )
