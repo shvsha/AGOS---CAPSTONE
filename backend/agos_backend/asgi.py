dafa9bcbf9a872bc5e0933e1df065537cfb0534c
SanaSha6 <nigosivan619@gmail.com>
2026-08-21 21:54:59 +0800

    fix: email smtp not sending otp code for forgot password

--- backend/agos_backend/asgi.py
+++ backend/agos_backend/asgi.py
@@ -1,3 +1,10 @@
+import socket
+
+_orig_getaddrinfo = socket.getaddrinfo
+def _getaddrinfo_ipv4_only(host, port, family=0, type=0, proto=0, flags=0):
+    return _orig_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
+socket.getaddrinfo = _getaddrinfo_ipv4_only
+
 import os
 
 from django.core.asgi import get_asgi_application
