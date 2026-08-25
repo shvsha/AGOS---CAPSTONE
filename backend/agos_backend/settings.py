a2e4fe88918ee8f3ec328ddb9d867bb25982f597
SanaSha6 <nigosivan619@gmail.com>
2026-08-26 00:34:58 +0800

    fix email

--- backend/agos_backend/settings.py
+++ backend/agos_backend/settings.py
@@ -265,19 +265,19 @@ SIMPLE_JWT = {
 # ------------------------------
 
 # Email Settings (local/ google smtp)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
-# Email Settings (Deploy/BREVO
-# EMAIL_BACKEND = 'anymail.backends.brevo.EmailBackend'
-# ANYMAIL = {
-#     'BREVO_API_KEY': os.getenv('BREVO_API_KEY'),
-# }
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 # EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+# Email Settings (Deploy/BREVO)
+EMAIL_BACKEND = 'anymail.backends.brevo.EmailBackend'
+ANYMAIL = {
+    'BREVO_API_KEY': os.getenv('BREVO_API_KEY'),
+}
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 
 # ----------------------------------
 
